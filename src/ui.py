d5478c7638bfc6d9d7d5d218b14ad5ed134a6195
Sergey Vartanov <me@enzet.ru>
2021-09-04 06:39:23 +0300

    Add simple UI

---
+++ src/ui.py
@@ -0,0 +1,30 @@
+import argparse
+
+from src.wikidata import MOON
+
+
+def parse_arguments(arguments: list[str]) -> argparse.Namespace:
+    """Parse command-line arguments."""
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-b",
+        "--body",
+        metavar="<integer>",
+        help=f"astronomical body Wikidata id without Q (e.g. {MOON} for Moon)",
+        type=int,
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        metavar="<path>",
+        help="output OSM XML file path",
+    )
+    parser.add_argument(
+        "-e",
+        "--extra",
+        metavar="<path>",
+        help="extra JSON data with node description",
+        nargs="*",
+    )
+    return parser.parse_args(arguments)
